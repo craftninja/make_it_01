b46653c1dc0be0cc225e9d5dbb1d563b518523b4
EmilyPlatzer <emilyplatzer@gmail.com>
2014-06-28 17:29:28 -0600

    User can see project show page

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -6,6 +6,17 @@ feature 'User can manage a list of projects' do
     click_on 'Add project'
     fill_in 'Project name', :with => 'Make t-shirt quilt'
     click_on 'Add project'
+    expect(page).to have_content('Look at all the stuff you want to do!')
+    expect(page).to have_content('Make t-shirt quilt')
+  end
+
+  scenario 'User can see project\'s show page' do
+    visit '/projects'
+    click_on 'Add project'
+    fill_in 'Project name', :with => 'Make t-shirt quilt'
+    click_on 'Add project'
+    click_on 'Make t-shirt quilt'
+    expect(page).to_not have_content('Look at all the stuff you want to do!')
     expect(page).to have_content('Make t-shirt quilt')
   end
 
